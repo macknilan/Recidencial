a8cd79366fac1535c14143e28532c7ab18e5c15b
macknilan <rodolfougaldeochoa@gmail.com>
2015-04-09 21:50:38 -0500

    Se quito el paquete sorl-thumbnail modificar para mejorar

--- userprofiles/models.py
+++ userprofiles/models.py
@@ -3,7 +3,6 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils.text import slugify
-from sorl.thumbnail import get_thumbnail
 
 # from django.conf import settings
 # from django.utils.translation import ugettext_lazy as _
@@ -43,7 +42,7 @@ class UserProfile(SlugMixin, models.Model):
         super(UserProfile, self).save(*args, **kwargs)
 
     def image_avatar_admin(self):
-        return '<img src="%s">' % get_thumbnail(self.avatar, '50x50', quality=80).url
+        return '<img src="%s">' % self.avatar.url
 
     image_avatar_admin.allow_tags = True
     image_avatar_admin.admin_order_field = 'avatar'
